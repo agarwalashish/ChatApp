c03ff581e43cdb8fa48b4bf057aad94b64333fd5
Ashish Agarwal <iamashish.agarwal@gmail.com>
2015-09-07 19:38:44 -0700

    Set uitableview cell insets to zero

--- RealTimePush/ChatRoomViewController.m
+++ RealTimePush/ChatRoomViewController.m
@@ -134,6 +134,26 @@
     return cell;
 }
 
+// forcing tableivew insets to 0
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Remove seperator inset
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+        [cell setSeparatorInset:UIEdgeInsetsZero];
+    }
+    
+    // Prevent the cell from inheriting the Table View's margin settings
+    if ([cell respondsToSelector:@selector(setPreservesSuperviewLayoutMargins:)]) {
+        [cell setPreservesSuperviewLayoutMargins:NO];
+    }
+    
+    // Explictly set your cell's layout margins
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+        [cell setLayoutMargins:UIEdgeInsetsZero];
+    }
+}
+
+
 -(UIView *) tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
     UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
     
